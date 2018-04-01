1edc9fed8b1731bcce64f87af4ff92e080f18ef8
Pronoy Chopra <pronoyc@gmail.com>
2018-04-01 03:10:34 -0700

    added a private slot method

--- qtExamples/PythonIPCTest/mainwindow.h
+++ qtExamples/PythonIPCTest/mainwindow.h
@@ -17,6 +17,9 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
     QPushButton *launchButton;
