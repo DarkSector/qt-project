272231c3e9c095f791cee85171f4f058e01ed870
Pronoy Chopra <pronoyc@gmail.com>
2018-04-01 02:45:30 -0700

    Added button and widget and added slot

--- qtExamples/HelloWorld/helloworldwidget.cpp
+++ qtExamples/HelloWorld/helloworldwidget.cpp
@@ -1,14 +1,33 @@
 #include "helloworldwidget.h"
 #include "ui_helloworldwidget.h"
+#include <QPushButton>
+#include <QDebug>
+#include <QRect>
+#include <QLabel>
 
 HelloWorldWidget::HelloWorldWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::HelloWorldWidget)
 {
+    // constructor
     ui->setupUi(this);
+    pushButton1 = new QPushButton("Press Me", this);
+    pushButton1->setGeometry(QRect(100, 100, 100, 100));
+    label = new QLabel("label1", this);
+    label->hide();
+    // add a slot
+    connect(pushButton1, SIGNAL (clicked()), this, SLOT (on_pushButton_clicked()));
 }
 
 HelloWorldWidget::~HelloWorldWidget()
+// destructor
 {
     delete ui;
+}
+
+void HelloWorldWidget::on_pushButton_clicked()
+{
+    label->setText("Hello World");
+    label->show();
+
 }
--- qtExamples/HelloWorld/helloworldwidget.h
+++ qtExamples/HelloWorld/helloworldwidget.h
@@ -2,6 +2,8 @@
 #define HELLOWORLDWIDGET_H
 
 #include <QWidget>
+#include <QPushButton>
+#include <QLabel>
 
 namespace Ui {
 class HelloWorldWidget;
@@ -9,14 +11,20 @@ class HelloWorldWidget;
 
 class HelloWorldWidget : public QWidget
 {
+    // Macro: MOC meta object creator
     Q_OBJECT
 
 public:
     explicit HelloWorldWidget(QWidget *parent = 0);
     ~HelloWorldWidget();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::HelloWorldWidget *ui;
+    QPushButton *pushButton1;
+    QLabel *label;
 };
 
 #endif // HELLOWORLDWIDGET_H
