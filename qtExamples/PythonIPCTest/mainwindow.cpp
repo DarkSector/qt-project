d8a07b343f01575e8c323c9ec64b5316f9d4676f
Pronoy Chopra <pronoyc@gmail.com>
2018-04-01 15:44:53 -0700

    Slots added for the 3 buttons

--- qtExamples/PythonIPCTest/mainwindow.cpp
+++ qtExamples/PythonIPCTest/mainwindow.cpp
@@ -2,16 +2,39 @@
 #include "ui_mainwindow.h"
 #include <QPushButton>
 #include <QDebug>
+#include "localserver.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    mLocalServer = new LocalServer(this);
 
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_exitButton_clicked()
+{
+    close();
+
+}
+
+void MainWindow::on_submitButton_clicked()
+{
+
+    mLocalServer->submit(ui->lineEdit->text());
+
+}
+
+void MainWindow::on_startButton_clicked()
+{
+    if(!mLocalServer->listen("myLocalServer")){
+        QMessageBox::critical(this, "Error", mLocalServer->errorString());
+    }
 }
