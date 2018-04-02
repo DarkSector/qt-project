8b202947e119f3eaf645eed24203f40cfbcb1913
Pronoy Chopra <pronoyc@gmail.com>
2018-04-02 16:45:20 -0700

    New API for histogram generation via numpy

---
+++ python/qtAPI/restapi/api.py
@@ -0,0 +1,10 @@
+
+
+class Histogram(object):
+
+    def __init__(self, image_location, histogram_type='rgb'):
+        self.image_location = image_location
+        self.histogram_type = histogram_type
+
+    def generate(self):
+        return None
