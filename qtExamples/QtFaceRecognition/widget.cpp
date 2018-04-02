1a415cff86d36c1bfa6556d62590a4ffd2ad2b81
Pronoy Chopra <pronoyc@gmail.com>
2018-04-01 20:24:41 -0700

    Added a scroll view on the gallery and added new slots

--- qtExamples/QtFaceRecognition/widget.cpp
+++ qtExamples/QtFaceRecognition/widget.cpp
@@ -1,5 +1,6 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QDebug>
 #include <QCamera>
 #include <QCameraViewfinder>
 #include <QCameraImageCapture>
@@ -14,7 +15,7 @@ Widget::Widget(QWidget *parent) :
     pCameraViewfinder = new QCameraViewfinder(this);
     pCameraImageCapture = new QCameraImageCapture(pCamera, this);
     pLayout = new QVBoxLayout;
-
+    pCamera->setViewfinder(pCameraViewfinder);
     pLayout->addWidget(pCameraViewfinder);
     ui->scrollArea->setLayout(pLayout);
 
@@ -34,5 +35,38 @@ void Widget::on_pushButton_clicked()
 void Widget::on_pushButton_2_clicked()
 {
     // Take picture here
+
+}
+
+void Widget::on_tabWidget_currentChanged(int index)
+{
+    qDebug() << QString("Current tab index is %1").arg(index);
+    switch(index){
+        case 0:
+            qDebug("Initializing camera");
+            pCamera->start();
+            break;
+//        case 1:
+//            qDebug("Gallery");
+//            pCamera->stop();
+//            break;
+//        case 2:
+//            qDebug("Analysis");
+//            pCamera->stop();
+//            break;
+        default:
+            qDebug("Stopping camera");
+            pCamera->stop();
+            break;
+    }
+}
+
+void Widget::on_pushButton_5_clicked()
+{
+
+}
+
+void Widget::on_pushButton_6_clicked()
+{
 
 }
