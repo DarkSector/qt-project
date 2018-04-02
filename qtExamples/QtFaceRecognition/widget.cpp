88b7c6abbf8793882808a4f79c52cead7824a468
Pronoy Chopra <pronoyc@gmail.com>
2018-04-01 23:51:50 -0700

    setEncoding added pre camera; Added a dialog box to select destination

--- qtExamples/QtFaceRecognition/widget.cpp
+++ qtExamples/QtFaceRecognition/widget.cpp
@@ -2,22 +2,39 @@
 #include "ui_widget.h"
 #include <QDebug>
 #include <QCamera>
+#include <QImageEncoderSettings>
 #include <QCameraViewfinder>
 #include <QCameraImageCapture>
 #include <QVBoxLayout>
+#include <QFileDialog>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    // Always start with 0
+    ui->tabWidget->setCurrentIndex(0);
+
+    // Initialize camera
     pCamera = new QCamera(this);
     pCameraViewfinder = new QCameraViewfinder(this);
     pCameraImageCapture = new QCameraImageCapture(pCamera, this);
+
+    // Add layout
     pLayout = new QVBoxLayout;
+
+    // Add link viewfinder with camera
     pCamera->setViewfinder(pCameraViewfinder);
+
+    // And add the viewfinder widget to the layout
     pLayout->addWidget(pCameraViewfinder);
+
+    // add layout to scroll area on the uI
     ui->scrollArea->setLayout(pLayout);
+
+    setEncoding();
+    pCamera->start();
 
 }
 
@@ -26,24 +43,13 @@ Widget::~Widget()
     delete ui;
 }
 
-void Widget::on_pushButton_clicked()
-{
-    // Terminate application
-    close();
-}
-
-void Widget::on_pushButton_2_clicked()
-{
-    // Take picture here
-
-}
-
 void Widget::on_tabWidget_currentChanged(int index)
 {
     qDebug() << QString("Current tab index is %1").arg(index);
     switch(index){
         case 0:
             qDebug("Initializing camera");
+            setEncoding();
             pCamera->start();
             break;
 //        case 1:
@@ -61,12 +67,32 @@ void Widget::on_tabWidget_currentChanged(int index)
     }
 }
 
-void Widget::on_pushButton_5_clicked()
+void Widget::on_applicationClose_clicked()
 {
-
+    close();
 }
 
-void Widget::on_pushButton_6_clicked()
+void Widget::on_takePicture_clicked()
 {
+    pCamera->searchAndLock();
+    pCameraImageCapture->capture(ui->pathField->text());
+    pCamera->unlock();
+}
 
+
+void Widget::setEncoding(){
+    pCameraImageCapture->setCaptureDestination(QCameraImageCapture::CaptureToFile);
+    QImageEncoderSettings imageEncoderSettings;
+    imageEncoderSettings.setCodec("image/png");
+    imageEncoderSettings.setResolution(720, 480);
+    pCameraImageCapture->setEncodingSettings(imageEncoderSettings);
+    pCamera->setCaptureMode(QCamera::CaptureStillImage);
+}
+
+void Widget::on_browseButton_clicked()
+{
+    QString dir = QFileDialog::getExistingDirectory(this, tr("Open Directory"),
+                                                 "/home",
+                                                 QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+    ui->pathField->setText(dir);
 }
