68d7b596858f4625b6fe6831680c99346838a5e9
Pronoy Chopra <pronoyc@gmail.com>
2018-04-02 23:06:03 -0700

    New view

--- python/qtAPIServer/restapi/views.py
+++ python/qtAPIServer/restapi/views.py
@@ -2,20 +2,26 @@
 import json
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from restapi.api import Histogram
+from api import Histogram
 
 
 class GenerateHistogramView(APIView):
 
-    def post(self, request, histogram_type):
+    def post(self, request):
         # get the image location
         image_location = request.data.get('image_location')
 
         # use opencv to create a histogram and return results
-        histogram = Histogram(image_location, histogram_type=histogram_type)
+        histogram = Histogram(image_location, 'rgb')
         data = histogram.generate()
-        return Response(json.dumps(data))
+        return Response(json.dumps({'foo': 'bar'}))
 
-    def get(self, request, histogram_type):
+    def get(self, request):
         return Response({'foo': 'bar'})
+
+
+class FaceDetection(APIView):
+
+    def post(self, request):
+
+        pass
